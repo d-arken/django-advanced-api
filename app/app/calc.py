f0f4d71a28502b4cdb0c4e9d8aa222894f9dedf6
Matheus Bortoletto <matheus.bortoletto@gadle.it>
2020-11-28 20:31:58 -0300

    TDD + Lint

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,2 +1,6 @@
 def add(x, y):
     return x + y
+
+
+def sub(x, y):
+    return x - y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,15 @@
 from django.test import TestCase
 
-from app.calc import add
+from app.calc import add, sub
+
 
 class CalcTests(TestCase):
     def test_add_numbers(self):
         """Test that two numbers are added"""
-        soma = add(3,8)
+        soma = add(3, 8)
         self.assertEqual(soma, 11)
+
+    def test_subtract_numbers(self):
+        """Test that two numbers are subtracted"""
+        subtraction = sub(8, 3)
+        self.assertEqual(subtraction, 5)
